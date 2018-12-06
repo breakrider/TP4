9e762904423dc3f6d6e61c81ebda88ef4bad6af8
max dion <maximumdion@hotmail.com>
2018-12-06 14:45:27 -0500

    debut

---
+++ TP4_concept/TP4_concept/Classe/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP4_concept.Classe
+{
+    public class Menu
+    {
+        private List<Mets> m_listMenu;
+        
+        public Menu()
+        {
+            m_listMenu = new List<Mets>();
+        }
+
+        public void ConsulterMenu()
+        {
+            foreach(Mets m in m_listMenu)
+            {
+
+            }
+        }
+    }
+}
---
+++ TP4_concept/TP4_concept/Classe/Mets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP4_concept.Classe
+{
+    public class Mets
+    {
+    }
+}
