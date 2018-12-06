a6c65113f08e2babdefa71268930642504810844
Jonathan Brazeau <jonathan.brazeau.94@gmail.com>
2018-12-06 14:47:19 -0500

    Fichiers

---
+++ TP4_concept/TP4_concept/objets/Central.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tp4.objets
+{
+    public class Central : Etablissement
+    {
+        public Central(String p_NomSuccursale, String p_Adresse, int p_Telephone)
+        {
+            this.NomSuccursale = p_NomSuccursale;
+            this.Adresse       = p_Adresse;
+            this.Telephone     = p_Telephone;
+            this.ListeEmployes = new List<Employe>();
+        }
+    }
+}
---
+++ TP4_concept/TP4_concept/objets/Etablissement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tp4.objets
+{
+    public abstract class Etablissement
+    {
+        private String        m_NomSuccursale;
+        private String        m_Adresse;
+        private int           m_Telephone;
+        private List<Employe> m_ListeEmployes;
+
+        public String NomSuccursale
+        {
+            get
+            {
+                return this.m_NomSuccursale;
+            }
+            protected set
+            {
+                this.m_NomSuccursale = value;
+            }
+        }
+
+        public String Adresse
+        {
+            get
+            {
+                return this.m_Adresse;
+            }
+            protected set
+            {
+                this.m_Adresse = value;
+            }
+        }
+
+        public int Telephone
+        {
+            get
+            {
+                return this.m_Telephone;
+            }
+            protected set
+            {
+                this.m_Telephone = value;
+            }
+        }
+
+        public List<Employe> ListeEmployes
+        {
+            get
+            {
+                return this.m_ListeEmployes;
+            }
+            protected set
+            {
+                this.m_ListeEmployes = value;
+            }
+        }
+
+        public void AjouterEmploye(Employe p_employe)
+        {
+            this.ListeEmployes.Add(p_employe);
+        }
+    }
+}
