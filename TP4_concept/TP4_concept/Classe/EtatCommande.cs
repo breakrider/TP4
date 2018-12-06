22d56937049c7b941c3c9c33753b6b94ec963896
Jonathan Brazeau <jonathan.brazeau.94@gmail.com>
2018-12-06 15:09:51 -0500

    Fichiers

---
+++ TP4_concept/TP4_concept/Classe/EtatCommande.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tp4_concept.Classe
+{
+    public enum EtatCommande
+    {
+        Envoyee,
+        Annulee,
+        Refusee,
+        AttentePreparation,
+        EnPreparation,
+        AttenteLivraison,
+        EnChemin,
+        Retournee,
+        Clot
+    }
+}
